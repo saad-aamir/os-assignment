8363ebf91496adf78b3ed288df6500e5c56ba80b
saad-aamir <saadaamir43@gmail.com>
2021-04-15 15:47:56 +0500

    Added code

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,27 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
+    print_str("Saad Aamir - 290527 - BCCS-9B");
+    print_str("\n\n");
+    print_set_color(PRINT_COLOR_LIGHT_BLUE, PRINT_COLOR_BLACK);
+    print_str("\n\n\n\n");
+    print_str("***         **      **              **      *************      ******************\n");
+    print_str("****        **      **              **      **                         **        \n");
+    print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("** **       **      **              **      **                         **        \n");
+    print_str("**  **      **      **              **      **                         **        \n");
+    print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
+    print_str("**   **     **      **              **      **                         **        \n");
+    print_str("**    **    **      **              **      *************              **        \n");
+    print_str("**     **   **      **              **                 **              **        \n");
+    print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("**      **  **      **              **                 **              **        \n");
+    print_str("**       ** **      **              **                 **              **        \n");
+    print_set_color(PRINT_COLOR_LIGHT_BLUE, PRINT_COLOR_BLACK);
+    print_str("**        ****      **              **                 **              **        \n");
+    print_str("**         ***      ******************      *************              **        \n");
+    
+
+}
